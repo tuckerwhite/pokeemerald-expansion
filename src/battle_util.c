9b5c97ee0a4813ae3a614345143d0360d6317c38
tuckerwhite <freklesmcoy@gmail.com>
2020-12-10 20:33:38 -0500

    Tweaking one more

--- src/battle_util.c
+++ src/battle_util.c
@@ -6565,7 +6565,7 @@ static u16 CalcMoveBasePower(u16 move, u8 battlerAtk, u8 battlerDef)
             basePower = 150;
         break;
     case EFFECT_SHIELD_SLAM:
-        basePower = gBattleMons[battlerAtk].defense;
+        basePower = (1 * GetBattlerTotalDefenseStat(battlerAtk)) + 1;
         if (basePower > 150)
             basePower = 150;
         break;
