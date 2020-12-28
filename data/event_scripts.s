b00df371631e483d735f16f63b6f63d2e63d06bf
tuckerwhite <freklesmcoy@gmail.com>
2020-12-28 13:21:49 -0500

    Altered Pokemon Center, fixed NewMap1 issue

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1060,3 +1060,6 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+
+	.include "data/maps/NewMap1/scripts.inc"
